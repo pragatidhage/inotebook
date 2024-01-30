ffbb40c956c83e877fd4f0d858b87b938e019f8c
Pragati Dhage <pragatidhage971@gmail.com>
2024-01-31 00:19:38 +0530

    created collection in thunderclient /created request for user creation in db

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 connectToMongo();
 
 const app = express()
-const port = 3000
+const port = 5000
 
 //middleware to hit the endpoint
 app.use(express.json())
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,27 +1,45 @@
-const express = require('express');
-const User = require('../models/User')
+const express = require("express");
+const User = require("../models/User");
 const router = express.Router();
-const { body ,validationResult} = require('express-validator');
+const { body, validationResult } = require("express-validator");
 
+//create a user using :POST "/api/auth/createuser". no login required
+router.post(
+  "/createuser",
+  [
+    body("email", "Enter valid email").isEmail(),
+    body("name", "Enter valid name").isLength({ min: 3 }),
+    body("password", "Password must be atleast five charecter").isLength({
+      min: 3,
+    }),
+  ],
+  async (req, res) => {
+    //if error then return bad req and errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-//create a user using :POST "/api/auth/"
-router.post('/',[
-   body('email','Enter valid email').isEmail(),
-   body('name','Enter valid name').isLength({min:3}),
-   body('password','Password must be atleast five charecter').isLength({min:3}),
-],(req,res)=>{
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+    //check wheather user with same email exists
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "User with this email already exists" });
+      }
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      });
+
+      res.json(user);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Some error occured")
+    }
   }
-  User.create({
-   name: req.body.name,
-   email: req.body.email,
-   password: req.body.password,
-  }).then(user=>res.json(user))
-  .catch(err=> {console.log(err)
-  res.json({error: 'please enter a unique value for email',message: err.message})})
+);
 
-})
-
-module.exports= router
+module.exports = router;
