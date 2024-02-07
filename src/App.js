b92aa1cf75fa2678ae4741e53d24f1f5a362b2a2
Pragati Dhage <pragatidhage971@gmail.com>
2024-02-07 23:59:46 +0530

    used context api to change state of component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import About from './components/About';
+import Home from './components/Home';
+import Navbar from './components/Navbar'
+import NoteState from './context/notes/NoteState'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is my react app</h1>
-    </div>
+    <>
+    <NoteState>
+      <Router>
+      <Navbar/>
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+
+        <Route exact path='/about' element={<About/>}/>
+      </Routes>
+      </Router>
+      </NoteState>
+    </>
   );
 }
 
