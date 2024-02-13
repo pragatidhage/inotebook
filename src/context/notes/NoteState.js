45a8f5d9fc51e046edcdee352007f9a23c19a134
Pragati Dhage <pragatidhage971@gmail.com>
2024-02-13 13:37:28 +0530

    added modal for editing of note

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -53,7 +53,21 @@ const NoteState = (props) => {
   };
 
   //delete a note
-  const deleteNote = (id) => {
+  const deleteNote = async (id) => {
+    //API call
+    const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token":
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjViZjY2MDU4MTNmNWMzOGNkYTI5ZmU4In0sImlhdCI6MTcwNzA0MjMwOX0.bN1g-ElN4tc8MS3PYfOUFYMzibYXndXOtlGuMHHDpho",
+      },
+      
+    });
+    const json = response.json();
+    console.log(json)
+
+    ////
     console.log("deleting note from" + id);
     const newNotes = notes.filter((note) => {
       return note._id !== id;
